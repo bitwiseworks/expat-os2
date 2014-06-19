3da7b8114ee9e8696f66aed9cd318f63bb89d707
Dmitriy Kuminov <coding@dmik.org>
2014-06-19 11:56:01 +0000

    expat: Make bootstrap script create missing auxiliary config files.
    git-svn-id: http://svn.netlabs.org/repos/ports/expat/trunk@770 8d6eb4b1-0faa-de11-b929-00e081727c95

--- buildconf.sh
+++ buildconf.sh
@@ -17,7 +17,11 @@ if test "$1" = "2" -a "$2" -lt "58" || test "$1" -lt "2"; then
 fi
 
 echo "Creating configure ..."
-${AUTOCONF:-autoreconf} -fvi
+${AUTORECONF:-autoreconf} -fvi
+
+# autoreconf 2.69 doesn't pass --install to libtoolize, do it by hand
+echo "Installing auxiliary config files ..."
+${LIBTOOLIZE:-libtoolize} -c -i
 
 # toss this; it gets created by autoconf on some systems
 rm -rf autom4te*.cache
